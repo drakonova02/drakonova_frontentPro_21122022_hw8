a8690ba7b8ef5ff2ed37db24e7d868c415839b01
alesiad <drakonova02@gmail.com>
2022-12-21 01:40:27 +0100

    create files for hw

---
+++ hw8/index.js
@@ -0,0 +1,13 @@
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+
+switch(true) {
+    case (numOrStr === null):
+        console.log('ви скасували');
+        break;
+    case (isNaN( +numOrStr )): 
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+}
